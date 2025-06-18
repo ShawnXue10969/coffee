59396b721343076648ba88201a3fb91f24c5d3cd
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2025-06-18 17:04:22 +1000

    dynamic routes

--- components/banner.client.tsx
+++ components/banner.client.tsx
@@ -11,9 +11,9 @@ export default function banner() {
       <div className="z-20 flex flex-col px-2 md:pt-12">
         <h1 className="my-2 flex-wrap">
           <span className="pr-2 text-white">Coffee</span>
-          <span className="text-gray-400">Connoisseur</span>
+          <span className="text-gray-900">Connoisseur</span>
         </h1>
-        <p className="font-sans text-xl font-semibold text-gray-400 md:mt-5 lg:text-2xl">
+        <p className="font-sans text-xl font-semibold text-gray-900 md:mt-5 lg:text-2xl">
           Discover your local coffee shops!
         </p>
 
