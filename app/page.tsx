7794cadf2159b58ac4b14eab4c3a6daa56dc24b1
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2025-06-18 15:12:14 +1000

    Create banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Banner from "@/components/banner.client";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Coffee</h1>
-        <h2 className="font-inter">The best coffee shops in town</h2>
+        <Banner />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
