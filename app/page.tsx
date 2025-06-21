4805a705ab275481f9b79ac583fbe5f1bda76765
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2025-06-21 13:02:39 +1000

    dummy data & img blur placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,55 @@ import Banner from "@/components/banner.client";
 import Card from "@/components/card.server";
 
 export default function Home() {
-  const coffeeStoreId = "dark-horse-coffee";
+  const coffeeStores = [
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a63516050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMiA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a63516050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMiA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a63516050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMiA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="mb-56">
+      <main className="mx-auto mt-10 max-w-6xl px-4">
         <Banner />
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
-          <Card
-            name="Dark Horse Coffee"
-            imgUrl=""
-            href={`/coffee-store/${coffeeStoreId}`}
-          ></Card>
+
+        <div className="mt-20">
+          <h2 className="mt-8 pb-8 text-4xl font-bold text-white">Brisbane Stores</h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+            {coffeeStores.map((coffeeStore, idx) => (
+              <Card
+                key={`${coffeeStore.name}-${idx}`}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${idx}`}
+              />
+            ))}
+          </div>
         </div>
       </main>
     </div>
