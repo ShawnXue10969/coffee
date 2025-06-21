814d14a8ef31088e628353a5b3546ced28d74fda
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2025-06-21 11:05:54 +1000

    card style update

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Banner />
-        <Card
-          name="Dark Horse Coffee"
-          imgUrl=""
-          href={`/coffee-store/${coffeeStoreId}`}
-        ></Card>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+          <Card
+            name="Dark Horse Coffee"
+            imgUrl=""
+            href={`/coffee-store/${coffeeStoreId}`}
+          ></Card>
+        </div>
       </main>
     </div>
   );
