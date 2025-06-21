6e2b9c9eaad534f3f5c7bed1a462cd8b6580a57c
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2025-06-21 15:26:04 +1000

    coffee fetch lib error handling

--- lib/coffee-stores.ts
+++ lib/coffee-stores.ts
@@ -1,7 +1,11 @@
 export const fetchCoffeeStores = async () => {
-  const response = await fetch(
-    `https://api.mapbox.com/search/searchbox/v1/forward?q=coffee&access_token=${process.env.MAPBOX_API}`
-  );
+  try {
+    const response = await fetch(
+      `https://api.mapbox.com/search/searchbox/v1/forward?q=coffee&access_token=${process.env.MAPBOX_API}`
+    );
 
-  return await response.json();
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching coffee stores:", error);
+  }
 };
